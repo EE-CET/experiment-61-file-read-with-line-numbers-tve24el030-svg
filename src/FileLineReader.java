8e2bedcfb592c189659a27fdab0034268173c8fc
tve24el030-svg <tve24el030@cet.ac.in>
2026-02-27 11:42:50 +0530

    code uploaded

--- src/FileLineReader.java
+++ src/FileLineReader.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileLineReader {
     public static void main(String[] args) {
         String fileName = "input.txt";
@@ -8,19 +12,22 @@ public class FileLineReader {
         // TODO: Print each line in the format "lineNumber: lineContent"
         // TODO: Catch and handle any IOExceptions that might occur
         
-        
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
 
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            String line;
-            int lineNumber = 1;
+		String line;
+		int lineNum=1;
 
-            while ((line = br.readLine()) != null) {
-                System.out.println(lineNumber + ": " + line);
-                lineNumber++;
-            }
+		while((line= br.readLine())!=null){
 
-        } catch (IOException e) {
-            System.out.println("Error reading file: " + e.getMessage());
-        }
+			System.out.println(lineNum+": "+line);
+			lineNum++;
+
+		}
+
+	}catch(IOException e){
+
+		System.out.println("File read error : "+e.getMessage());
+
+	}
     }
 }
