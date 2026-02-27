426d34db7224f120d4e29e56b9233892671448d0
tve24el030-svg <tve24el030@cet.ac.in>
2026-02-27 09:48:58 +0530

    code uploaded

--- src/FileLineReader.java
+++ src/FileLineReader.java
@@ -1,7 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class FileLineReader {
     public static void main(String[] args) {
         String fileName = "input.txt";
@@ -13,5 +9,18 @@ public class FileLineReader {
         // TODO: Catch and handle any IOExceptions that might occur
         
         
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            int lineNumber = 1;
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(lineNumber + ": " + line);
+                lineNumber++;
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
     }
 }
